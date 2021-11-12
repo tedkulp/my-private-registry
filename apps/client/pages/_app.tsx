723588f91367657ca5d7f72912bb0b9384d907f4
Ted Kulp <ted@tedkulp.com>
2021-11-12 18:01:09 -0500

    nx-ified

---
+++ apps/client/pages/_app.tsx
@@ -0,0 +1,26 @@
+import './styles.css';
+
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+
+function CustomApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Welcome to client!</title>
+      </Head>
+      <div className="app">
+        <header className="flex">
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img src="/nx-logo-white.svg" alt="Nx logo" width="75" height="50" />
+          <h1>Welcome to client!</h1>
+        </header>
+        <main>
+          <Component {...pageProps} />
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default CustomApp;
